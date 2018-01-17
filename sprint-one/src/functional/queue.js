8782a7bbc0a9925ed1643035db8b33d5b76c9c99
Michael Durfey <mdurfey@me.com>
2018-01-17 15:00:49 -0800

    progress

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,23 @@
 var Queue = function() {
+  var size = 0;
   var someInstance = {};
-
   // Use an object with numeric keys to store values
   var storage = {};
-
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size++;
   };
-
   someInstance.dequeue = function() {
+    if (size > 0) {
+      size--;
+      var bottomItem = storage[size];
+      delete storage[size];
+      return topItem;
+    }
   };
-
   someInstance.size = function() {
+    return size;
   };
-
   return someInstance;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,23 @@
 var Stack = function() {
+  var size = 0;
   var someInstance = {};
-
   // Use an object with numeric keys to store values
   var storage = {};
-
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
   };
-
   someInstance.pop = function() {
+    if (size > 0){
+      size--;
+      var topItem = storage[size];
+      delete storage[size];
+      return topItem;
+    }
   };
-
   someInstance.size = function() {
+    return size;
   };
-
   return someInstance;
 };
