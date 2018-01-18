dd5049a6330487d6800b41079ec039d395e34842
michaeldurfey <mdurfey@me.com>
2018-01-18 13:44:41 -0800

    linked list progress

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,33 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.length = 0;
 
-  list.addToTail = function(value) {
+  list.addToTail = function(value) {  
+    let temp = Node(value);
+    console.log(temp)
+    if (list.tail) {
+      list.tail.next = temp;
+      list.tail = temp;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head) {
+      let temp = list.head;
+      if (list.head.next) {
+        list.head = list.head.next;
+      } else {
+        list.head = null;
+      }
+      list.length--; 
+      return temp;
+    } else {
+      list.tail = null;
+    }
   };
-
   list.contains = function(target) {
+    
   };
 
   return list;
