65d1e08811c091775b56dd4911f3c0bce9779cb8
Michael Durfey <mdurfey@me.com>
2018-01-19 08:48:11 -0800

    finished graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,32 +3,31 @@
 // Instantiate a new graph
 var Graph = function() {
   this.nodes = {};
-  this.edges = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this.nodes[node] = node;
+  if (!this.contains(node)) { this.nodes[node] = {edges: {}}; }
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  for (let x in this.nodes) {
-    if (this.nodes[x] === node) {
-      return true;
-    }
-  }
-  return false;
+  return !!this.nodes[node];
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  delete this.nodes[node];
+  if (this.contains(node)) {
+    for (let x in this.nodes[node].edges) {
+      this.removeEdge(node, x);
+    }
+    delete this.nodes[node];
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  if (this.edges[fromNode] === toNode) {
+  if (this.nodes[fromNode].edges[toNode]) {
     return true;
   } else {
     return false;
@@ -37,21 +36,26 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  this.edges[fromNode] = toNode;
+  if (this.contains(fromNode) && this.contains(toNode)) {
+    this.nodes[fromNode].edges[toNode] = true;
+    this.nodes[toNode].edges[fromNode] = true;
+  }
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  if (this.edges[fromNode] === toNode) {
-    delete this.edges[fromNode];
+  if (this.contains(fromNode) && this.contains(toNode)) {
+    delete this.nodes[fromNode].edges[toNode];
+    delete this.nodes[toNode].edges[fromNode];
   }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (let x in this.nodes) {
+    cb(x);
+  }
 };
-
-console.log(new Graph())
 
 /*
  * Complexity: What is the time complexity of the above functions?
