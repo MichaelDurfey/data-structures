235bf764b8b67ad5adbc1e5d0c01ee3059167464
michaeldurfey <mdurfey@me.com>
2018-01-19 12:25:25 -0800

    completed set and hash

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,21 +1,54 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.count = 0;
+// console.log(LimitedArray)
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tuple = [k, v];
+  var bucket = this._storage.get(index);
+  if (!bucket) {
+    this._storage.set(index, [tuple]);
+  } else {
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i][1] = v;
+      } else {
+        bucket.push(tuple);
+      }
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if (bucket) {
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i] && bucket[i][0] === k) {
+        return bucket[i][1];
+      }
+    }
+  }
+  return;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if (bucket) {
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        delete bucket[i];
+      }
+    }
+  } else {
+    return;
+  }
 };
+
 
 
 
